c21c9994266edc2a1be968ef1625038bc5d130df
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-01-31 11:47:22 -0300

    Plop config

---
+++ generators/plopfile.js
@@ -0,0 +1,24 @@
+export default function (plop) {
+  plop.setGenerator("component", {
+    description: "Create a component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What is your component name?",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
+        templateFile: "templates/component.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.module.css",
+        templateFile: "templates/module.css.hbs",
+      },
+    ],
+  });
+}
